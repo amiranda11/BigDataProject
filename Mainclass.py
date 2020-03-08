bfca3253cd465245f5304419be2629baeb3090be
Andrea Miranda <andrea.miranda1@upr.edu>
2020-03-08 00:30:30 -0400

    type functions

--- Mainclass.py
+++ Mainclass.py
@@ -18,9 +18,7 @@ with open(file_name) as File:
     for row in reader:
         file_tweets.append(row)
         
-'''
-    for loop saves every sentences
-'''
+#for loop saves every sentences in sentence[]
 
 sentences = [] #Contains every sentences of each row
 counter = 0
@@ -28,13 +26,15 @@ for i in file_tweets:
     sentences.append(file_tweets[counter]['ï»¿text'])
     counter+= 1
 
+    
+"""
 index = 0
 for i in sentences:
     rst = i.split(" ")
     print(rst)
     index +=1
 
-   
+"""
 #Text Analysis
 """
     Disruption Type: 
@@ -47,80 +47,99 @@ for i in sentences:
         actual disruption - 1
         not disruption - 0
 
-
-tweetlistHR = {'hurricane': [], 'water': [], 'power': [], 'communication': [], 'wastewater': [], 'transportion':[], 'other': []}
-
-tweetlistHR['hurricane'] = ["hurricane Irma", "hurricane"]
-tweetlistHR['water'] = ["bottle of water", "drinking water", "water", "bottled water"]
-tweetlistHR['power'] = ["power off", "power down", "lost power", "fallen power service", 
-                "fallen power cables", "fallen power spot", "power back", "power is back",
-                ]
 """
 #Identificando el tipo encontrando la palabra clave
 def evaluate_text(s):
     type = []
     index = 0
-    sentce =s.split()
+    sentce = s.split(" ", ".")
     for word in sentce:
         if(word == 'power'):
             type = analyzePower(sentce, index)
+        elif (word == 'internet' or 'signal' or 'tv'):
+            type = analyzeCommunication(sentce, index)
         elif (word == 'water'):
             type = analyzeWater(sentce, index)
-        elif (word == 'wastewater'):
-            type = analyzeWater(sentce, index)
+        elif (word == 'wastewater' or 'drainage' or 'evac' or 'evacuation'):
+            type = analyzeWasteWater(sentce, index)
         elif(word == 'hurricane'):
-            type = analyzeHurricane(sentce, index)
+            type = analyzeOther(sentce, index)
         elif( word == 'Irma'):
-            type = analyzeHurricane(sentce, index)
-        elif(word == 'transportation'):
+            type = analyzeOther(sentce, index)
+        elif(word == 'transportation' or 'trafic'or 'vehicular'):
             type = analyzeTransportation(sentce, index)
         elif(word == 'bridge'):
             type = analyzeTransportation(sentce, index)
         elif(word == 'road'):
             type = analyzeTransportation(sentce, index)
+        
         else:
             type = analyzeOther(sentce, index)
         index += 1
     return type
 
 
-
+# Power - 1
 def analyzePower(s, index):
-    if(s[index-1 or s[index+1]] == "not" or "off or down"):
+    if(s[index-1] or s[index+1] == "not" or "off" or "down" or "fallen" or "cables" or "service" or "lost"):
         return [1,1]
+    elif(s[index-1] or s[index+1] == "back"):
+        return [1,0]
+    elif(s[index-1] or s[index+1] == "back"):
+        return [1,0]
+    elif(s[index-1] == "have" or s[index+1] == "back"):
+        return [1,0]
     else:
-        return [1, 0]
+        return [7,0]
 
-          
+# Communication - 2   
+def analyzeCommunication(s, index):
+    if(s[index-1] or s[index+1] == "no" or "issue"):
+        return [2, 1]
+    else:
+        return [2, 0]
+
+# Water - 3
 def analyzeWater(s, index):
-    return [1, 0]
-    
-def analyzeTransportation(s, index):
-    return [1, 0]
-    
-def analyzeHurricane(s, index):
-    return [1, 0]
-    
-def analyzeOther(s, index):
-    return [1, 0]
-
-
-def assign_type(s):
-    if s == 'power':
-        return 1
-    elif s == 'communication':
-        return 2
-    elif s == 'water':
-        return 3
-    elif s == 'wastewater':
-        return 4
-    elif s == 'transportation':
-        return 5
-    elif s == 'other':
-        return 6
+    if(s[index -2] or s[index-1] == "bottle" or " bottled"):
+        if(s[index -3] == "last"):
+            return [3,1]
     else:
-        return 7
+        return [3, 0]
+# Wastewater - 4   
+# Mejorar 
+def analyzeWasteWater(s, index):
+    if(s[index -2] or s[index-1] == "bottle" or " bottled"):
+        if(s[index -3] == "last"):
+            return [3,1]
+    else:
+        return [3, 0]
 
+#Transportation - 5    
+def analyzeTransportation(s, index):
+    if(s[index] == "bridge"):
+        if(s[index-1] or s[index+1] == "closed" or "collapsed" or "fallen"):
+            return [5,1]
+        else:
+            return[5,0]
+    elif(s[index] == "traffic"):
+        if(s[index-1] or s[index+1] == "unnecessary" or "congestion" or "fallen"):
+            return [5,1]
+        else:
+            return[5,0]
+    elif(s[index] == "highway"):
+        return [5, 0]
+    else:
+        return [5, 0]
+    
+#Other but hurricanre relate- 6; Not hurricane relate - 7
+def analyzeOther(s, index):
+    if(s[index-1] or s[index+1] == "not" or "off or down" or "back"):
+        return [6,1]
+    elif(s[index-1] or s[index+1] == "fallen" or "cables" or "service"):
+        return [6,0]
+    else:
+        return [7, 0]
 
 '''
 Array que tiene en el indice [0] el type y en el indice [1] el status
