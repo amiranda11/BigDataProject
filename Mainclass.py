03af04fe6018abcc765af0edc1a9efa29c0cf5ba
Andrea Miranda <andrea.miranda1@upr.edu>
2020-03-09 12:42:05 -0400

    fixing troubles

--- Mainclass.py
+++ Mainclass.py
@@ -26,15 +26,10 @@ for i in file_tweets:
     sentences.append(file_tweets[counter]['ï»¿text'])
     counter+= 1
 
-    
-"""
-index = 0
-for i in sentences:
-    rst = i.split(" ")
-    print(rst)
-    index +=1
+#MainClass 
 
-"""
+    
+
 #Text Analysis
 """
     Disruption Type: 
@@ -52,94 +47,110 @@ for i in sentences:
 def evaluate_text(s):
     type = []
     index = 0
-    sentce = s.split(" ", ".")
+    sentce = s.split(" ")
+    blocked = [ '' ,'me' , 'you' , 'us' , 'we' , 'I' , 'are' , 'if', 'the' , 'or' , 'in', 'are' , 'is' , '!' , '?' , '@']
     for word in sentce:
+        #Ignoring me, the, of, /n or ''
+        for i in blocked:
+            if(word == i):
+            continue
+        #Analyzing the key words
         if(word == 'power'):
-            type = analyzePower(sentce, index)
-        elif (word == 'internet' or 'signal' or 'tv'):
-            type = analyzeCommunication(sentce, index)
+            type = analyzePower(s,sentce, index)
+        elif (word == 'internet'):
+            type = analyzeCommunication(s,sentce, index)
+        elif (word == 'signal'):
+            type = analyzeCommunication(s,sentce, index)
+        elif (word == 'tv'):
+            type = analyzeCommunication(s,sentce, index)
         elif (word == 'water'):
-            type = analyzeWater(sentce, index)
-        elif (word == 'wastewater' or 'drainage' or 'evac' or 'evacuation'):
-            type = analyzeWasteWater(sentce, index)
-        elif(word == 'hurricane'):
-            type = analyzeOther(sentce, index)
-        elif( word == 'Irma'):
-            type = analyzeOther(sentce, index)
-        elif(word == 'transportation' or 'trafic'or 'vehicular'):
-            type = analyzeTransportation(sentce, index)
+            type = analyzeWater(s, sentce, index)
+        elif (word == 'wastewater'):
+            type = analyzeWasteWater(s, sentce, index)
+        elif (word == 'drainage' ):
+            type = analyzeWasteWater(s, sentce, index)
+        elif (word == 'evac'):
+            type = analyzeWasteWater(s, sentce, index)
+        elif (word == 'evacuation'):
+            type = analyzeWasteWater(s, sentce, index)
+        elif(word == 'transportation'):
+            type = analyzeTransportation(s, sentce, index)
+        elif(word == 'vehicular'):
+            type = analyzeTransportation(s, sentce, index)
+        elif(word == 'trafic'):
+            type = analyzeTransportation(s, sentce, index)
         elif(word == 'bridge'):
-            type = analyzeTransportation(sentce, index)
+            type = analyzeTransportation(s, sentce, index)
         elif(word == 'road'):
-            type = analyzeTransportation(sentce, index)
-        
+            type = analyzeTransportation(s, sentce, index)
         else:
-            type = analyzeOther(sentce, index)
-        index += 1
+            pass
+    #type = analyzeOther(s, sentce)
+    index+=1
     return type
+        
 
 
 # Power - 1
-def analyzePower(s, index):
+def analyzePower(txt, s, index):
     if(s[index-1] or s[index+1] == "not" or "off" or "down" or "fallen" or "cables" or "service" or "lost"):
-        return [1,1]
+        return [txt, 1,1]
     elif(s[index-1] or s[index+1] == "back"):
-        return [1,0]
+        return [txt, 1,0]
     elif(s[index-1] or s[index+1] == "back"):
-        return [1,0]
+        return [txt, 1,0]
     elif(s[index-1] == "have" or s[index+1] == "back"):
-        return [1,0]
+        return [txt, 1,0]
     else:
-        return [7,0]
+        return [txt, 7,0]
 
 # Communication - 2   
-def analyzeCommunication(s, index):
+def analyzeCommunication(txt, s, index):
     if(s[index-1] or s[index+1] == "no" or "issue"):
-        return [2, 1]
+        return [txt, 2, 1]
     else:
-        return [2, 0]
+        return [txt, 2, 0]
 
 # Water - 3
-def analyzeWater(s, index):
+def analyzeWater(txt, s, index):
     if(s[index -2] or s[index-1] == "bottle" or " bottled"):
         if(s[index -3] == "last"):
-            return [3,1]
+            return [txt, 3,1]
     else:
-        return [3, 0]
+        return [txt, 3, 0]
 # Wastewater - 4   
 # Mejorar 
-def analyzeWasteWater(s, index):
+def analyzeWasteWater(txt,s, index):
     if(s[index -2] or s[index-1] == "bottle" or " bottled"):
         if(s[index -3] == "last"):
-            return [3,1]
+            return [txt, 3,1]
     else:
-        return [3, 0]
+        return [txt, 3, 0]
 
 #Transportation - 5    
-def analyzeTransportation(s, index):
+def analyzeTransportation(txt,s, index):
     if(s[index] == "bridge"):
         if(s[index-1] or s[index+1] == "closed" or "collapsed" or "fallen"):
-            return [5,1]
+            return [txt, 5,1]
         else:
-            return[5,0]
+            return[txt, 5,0]
     elif(s[index] == "traffic"):
         if(s[index-1] or s[index+1] == "unnecessary" or "congestion" or "fallen"):
-            return [5,1]
+            return [txt, 5,1]
         else:
-            return[5,0]
+            return[txt, 5,0]
     elif(s[index] == "highway"):
-        return [5, 0]
+        return [txt, 5, 0]
     else:
-        return [5, 0]
+        return [txt, 5, 0]
     
 #Other but hurricanre relate- 6; Not hurricane relate - 7
-def analyzeOther(s, index):
-    if(s[index-1] or s[index+1] == "not" or "off or down" or "back"):
-        return [6,1]
-    elif(s[index-1] or s[index+1] == "fallen" or "cables" or "service"):
-        return [6,0]
-    else:
-        return [7, 0]
+def analyzeOther(txt, s):
+    for word in s:
+        if(word == "Hurricane" or "Irma"):
+            return [txt, 6, 0]
+        else:
+            return [txt, 7, 0]
 
 '''
 Array que tiene en el indice [0] el type y en el indice [1] el status
@@ -147,17 +158,26 @@ La funcion del output tiene que evaluar la lista y por cada i escribir
 en el csv file el texto, el type y el status
 
 '''
-#ouputResult = [[s, assign_type(s), assign_status(s)]]
+#outputResult = [[s, assign_type(s), assign_status(s)]]
+outputResult = []
+#Main
+index = 0
+for i in sentences:
+    outputResult.append(evaluate_text(i))
 
-ouputResult = []
+
+for i in outputResult:
+    print (i)
 
 #output code
 
+'''
 with open('outputResults.csv', mode='w') as csv_file:
     fieldnames = ['ï»¿text', 'disruption_type', 'disruption_status']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     count = 0
     writer.writeheader()
-    for results in ouputResult:
-        writer.writerow({'ï»¿text': ouputResult[count][0], 
-        'disruption_type': ouputResult[count][1], 'disruption_status': ouputResult[count][2]})
+    for results in outputResult:
+        writer.writerow({'ï»¿text': outputResult[count][0], 
+        'disruption_type': outputResult[count][1], 'disruption_status': outputResult[count][2]})
+'''
